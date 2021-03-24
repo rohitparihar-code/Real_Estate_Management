a4e8d00654061080f03ce5986c4c539e6fabc507
rohitparihar-code <rohit.191cs245@nitk.edu.in>
2021-03-24 23:23:30 +0530

    Added small filter functionality, Sorts the properties according to selected filter

--- server.js
+++ server.js
@@ -20,18 +20,26 @@ app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 
+// ROUTES Created (For Trial Run)
+var filter = 0; // Used to display corresponding text as heading
+/* 
+    filter = 0 for home page
+    1 for area filter
+    2 for Cost filter
+    3 for BHK filter
+    4 for date filter
+*/
+
 app.get('/',function(req,res) {
-    
+    filter = 0;
     // Code for Card Test Run
     DB.query("select * from property", (err, rows) => {
         if(err) 
          throw err;
         // console.log(rows);
-        res.render('index.ejs', {rows: rows});
+        res.render('index.ejs', {rows: rows, filter: filter});
     });
 });
-
-// ROUTES Created (For Trial Run)
 
 app.get('/login', function(req, res) {
     res.render('signup.ejs');
@@ -52,6 +60,23 @@ app.get('/wishlist', function(req, res) {
 
 app.get('/feedback', function(req, res) {
     res.render('feedback.ejs')
+});
+
+app.get('/filter-area', function(req, res) {
+    filter = 1;
+    DB.query('select * from property order by area', (err, rows) => {
+        if( err ) throw err;
+        // console.log(rows);
+        res.render('index', {rows: rows, filter: filter});
+    });
+});
+
+app.get('/filter-cost', function(req, res) {
+    filter = 2;
+    DB.query('select * from property order by cost', (err, rows) => {
+        if( err ) throw err;
+        res.render('index', {rows: rows, filter: filter}); 
+    });
 });
 // Routes End
 
