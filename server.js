7bc0f8c952dd49eb3988a49141194702bc4bd0e4
Aditya R Rudra <adityaberasia@gmail.com>
2021-03-28 01:28:30 +0530

    Routes added.

--- server.js
+++ server.js
@@ -3,15 +3,21 @@ require('dotenv').config({
 });
 
 const express = require('express');
-// const morgan = require('morgan');
-// const _ = require('lodash');
+const morgan = require('morgan');
+const _ = require('lodash');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const DB = require('./config/database');
-const auth = require('./utility/authentication');
+const util = require('./utility');
+const { render } = require('ejs');
 
 const app = express();
 
+const secret = process.env.SECRET_KEY;
+app.set('secretKey', secret);
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 // app.use(cors());
@@ -30,38 +36,44 @@ var filter = 0; // Used to display corresponding text as heading
     4 for date filter
 */
 
-app.get('/',function(req,res) {
-    filter = 0;
-    // Code for Card Test Run
-    DB.query("select * from property", (err, rows) => {
-        if(err) 
-         throw err;
-        // console.log(rows);
-        res.render('index.ejs', {rows: rows, filter: filter});
-    });
-});
+// Utilities for different operations
+const auth = util.authentication;
+const landing = util.landing;
+const wishlist = util.wishlist;
+const feedback = util.feedback;
+const search = util.search;
 
-app.get('/login', function(req, res) {
-    res.render('signup.ejs');
-});
 
+// Routes BEGIN
+app.get('/', auth.validateUser, landing.renderIndexPage);
+
+// Authentication
+app.get('/login', auth.inputLoginCredentials);
 app.post('/login', auth.login);
 
+app.post('/signup', auth.signup);
+
+app.post('/logout', auth.logout);
+
+
+// Comparison
 app.get('/compare', function(req, res) {
     res.render('compare_page.ejs');
 });
 
-app.get('/wishlist', function(req, res) {
-    DB.query('select * from property', (err, rows) => {
-        if(err) throw err;
-        res.render('wishlist_page.ejs', {rows: rows});
-    });
-});
+// Wishlist
+app.get('/wishlist', auth.validateUser, wishlist.wishList);
+app.post('/wishlist', auth.validateUser, wishlist.addToWishlist);
+app.post('/removeFromWishlist', auth.validateUser, wishlist.removeFromWishlist);
 
-app.get('/feedback', function(req, res) {
-    res.render('feedback.ejs')
-});
+// Feedback
+app.get('/feedback', auth.validateUser, feedback.receiveFeedback);
+app.post('/feedback', auth.validateUser, feedback.submitFeedback);
 
+// Search
+app.post('/search', search.search, landing.renderIndexPage);
+
+/*
 app.get('/filter-area', function(req, res) {
     filter = 1;
     DB.query('select * from property order by area', (err, rows) => {
@@ -77,7 +89,9 @@ app.get('/filter-cost', function(req, res) {
         if( err ) throw err;
         res.render('index', {rows: rows, filter: filter}); 
     });
-});
+});*/
+
+
 // Routes End
 
 
